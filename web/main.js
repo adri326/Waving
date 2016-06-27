44eb6e8c47c186dc7275c803ba021625afa7b4a7
Adrien Burgun <adrien.burgun@orange.fr>
2016-06-27 08:51:58 +0200

    Create main.js

---
+++ web/main.js
@@ -0,0 +1,11 @@
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext2D();
+
+class Ball {
+  constructor(x_, y_, s) {
+    this.x = x_;
+    this.y = y_;
+    this.size = s;
+ }
+}
+
