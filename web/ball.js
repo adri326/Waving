a07b64ecad9c2dd81138be9b478d97e1861ad344
Adrien Burgun <adrien.burgun@orange.fr>
2016-06-28 08:35:24 +0200

    Update ball.js

--- web/ball.js
+++ web/ball.js
@@ -1,7 +1,13 @@
+class Ball {
+  constructor(x_, y_, s) {
+    this.x = x_;
+    this.y = y_;
+    this.size = s;
+  }
   function getDist(float p1, float p2) {
-    return sqrt((p1-x)*(p1-x)+(p2-y)*(p2-y))-size/2;
+    return sqrt((p1-this.x)*(p1-this.x)+(p2-this.y)*(p2-this.y))-this.size/2;
   }
   function isTouching(float p1, float p2, float p3) {
-    return sqrt((p1-x)*(p1-x)+(p2-y)*(p2-y))-p3<=size/2;
+    return sqrt((p1-this.x)*(p1-this.x)+(p2-this.y)*(p2-this.y))-p3<=this.size/2;
   }
 }
