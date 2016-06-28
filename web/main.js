694212cd4451375a8eeb8c5c4e076263ca52b565
Adrien Burgun <adrien.burgun@orange.fr>
2016-06-28 08:34:19 +0200

    Update main.js

--- web/main.js
+++ web/main.js
@@ -1,13 +1,6 @@
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext2D();
 
-class Ball {
-  constructor(x_, y_, s) {
-    this.x = x_;
-    this.y = y_;
-    this.size = s;
- }
-}
 
 function onKeyDown(e) {
   
